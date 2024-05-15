4411acff63b28174e3b58a65391a922b12bf5f15
svtter <svtter@qq.com>
2024-05-15 18:01:58 +0800

    feat: add save to disk.

--- src/meterviewer/files.py
+++ src/meterviewer/files.py
@@ -1,6 +1,12 @@
+import typing as t
 import numpy as np
 
 
 def save_to_disk(filename: str, data: np.ndarray):
     with open(filename, "wb") as f:
         np.save(f, data)
+
+
+def load_from_disk(filename, constraint: t.Callable):
+    with open(filename, "rb") as f:
+        return np.load(f)
