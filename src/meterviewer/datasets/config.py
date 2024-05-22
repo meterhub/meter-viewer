f8cb7672510e14e6820f0330f283ed366072f7ac
svtter <svtter@qq.com>
2024-05-22 08:58:06 +0800

    feat: add read_single_digit.

--- src/meterviewer/datasets/config.py
+++ src/meterviewer/datasets/config.py
@@ -1,26 +1,41 @@
 # read dataset from file system.
 from pathlib import Path as P
+from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 import typing as t
+
 from meterviewer import types as T, func as F
 
 
+@dataclass
 class RectO(object):
-    xmin: str
-    ymin: str
-    xmax: str
-    ymax: str
+    xmin: str = ""
+    ymin: str = ""
+    xmax: str = ""
+    ymax: str = ""
+
+    def check(self):
+        # assert rect.xmin != "" and rect.ymin != "" and rect.xmax != "" and rect.ymax != "", rect
+        assert self.xmin != "" and self.ymin != "" and self.xmax != "" and self.ymax != "", self
 
     def to_dict(self) -> T.Rect:
         return {"xmin": self.xmin, "ymin": self.ymin, "xmax": self.xmax, "ymax": self.ymax}
 
+    def __str__(self):
+        return f"RectO({self.xmin}, {self.ymin}, {self.xmax}, {self.ymax})"
 
-def read_xml(filename: P) -> t.Tuple[str, T.Rect]:
+    def __repr__(self):
+        return self.__str__()
+
+
+def read_xml(filename: P, read_func: t.Callable[[t.Any], t.Any]):
     # 解析 XML 文件
     tree = ET.parse(filename)
     root = tree.getroot()
+    return read_func(root)
 
-    # 遍历 XML 树
+
+def read_rect(root) -> t.Tuple[str, T.Rect]:
     val, rect_dict = "", RectO()
     for child in root:
         # find object
@@ -38,8 +53,63 @@ def read_xml(filename: P) -> t.Tuple[str, T.Rect]:
     return val, rect_dict.to_dict()
 
 
+def read_single_digit(root: t.Iterable):
+    def find_no(node) -> t.Tuple[int, RectO]:
+        no = -1
+        rect = RectO()
+        for subchild in node:
+            if subchild.tag == "no":
+                no = int(subchild.text.strip())
+            else:
+                rect = set_rect(rect, subchild)
+        rect.check()
+        assert no != -1, 'cannot find no number'
+        return no, rect
+
+    def set_rect(rect: RectO, sub) -> RectO:
+        assert hasattr(rect, sub.tag), (sub.tag, sub.text)
+        setattr(rect, sub.tag, sub.text)
+        assert getattr(rect, sub.tag) != ""
+        return rect
+
+    def num_check():
+        seta = {0, 1, 2, 3, 4, 5}
+        setb = set()
+
+        def is_valid():
+            return seta == setb, (seta, setb)
+
+        def set_num(no):
+            setb.add(no)
+
+        return set_num, is_valid
+
+    digit_rect = [RectO() for _ in range(6)]
+    set_num, is_valid = num_check()
+    is_loop, set_loop = F.looped()
+
+    for child in root:
+        if child.tag == "digit":
+            no, rect = find_no(child)
+            _ = set_loop(), set_num(no)
+            digit_rect[no] = rect
+
+    assert is_loop(), "node has no child"
+    cond, _ = is_valid()
+    assert cond
+
+    # return digit_rect, root, find_no, find_rect
+    return digit_rect
+
+
+def get_single_digit(filename: P) -> t.Tuple[str, T.Rect]:
+    val, _ = read_xml(filename, read_single_digit)
+    pos = read_xml(filename, read_single_digit)
+    return val, pos
+
+
 def get_rectangle(filename: P) -> T.Rect:
-    _, rect = read_xml(filename)
+    _, rect = read_xml(filename, read_rect)
     return rect
 
 
@@ -53,4 +123,4 @@ def get_xml_config_path(img_path: P) -> P:
 
 
 def get_xml_config(img_path: P) -> t.Tuple[str, T.Rect]:
-    return read_xml(get_xml_config_path(img_path))
+    return read_xml(get_xml_config_path(img_path), read_rect)
--- src/tests/test_datasets.py
+++ src/tests/test_datasets.py
@@ -1,8 +1,20 @@
-from meterviewer.datasets import config
+from meterviewer.datasets import config, dataset
+
+
+def test_read_single_digit(root_path):
+    p = config.read_xml(root_path / r"lens_6/XL/XL/M1L1XL" / "config" / "res.xml", config.read_single_digit)
+
+    assert len(p) == 6
+    p: list[config.RectO]
+    assert p[0].xmax == "105", p[0]
+
+
+def test_exist_dataset(root_path):
+    assert dataset.path(root_path, r"M1L3XL").exists()
 
 
 def test_read_xml(root_path):
-    v, r = config.read_xml(root_path / r"lens_6/XL/XL/M1L3XL" / "baocun" / "2018-11-23-12-16-01.xml")
+    v, r = config.read_xml(root_path / r"lens_6/XL/XL/M1L3XL" / "baocun" / "2018-11-23-12-16-01.xml", config.read_rect)
     assert v == "000994"
     assert r.get("xmin") == "79"
 
