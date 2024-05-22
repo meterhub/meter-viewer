cf68348cc477b46e784e0813c6883838c577fed4
svtter <svtter@qq.com>
2024-05-22 09:21:47 +0800

    feat: finish cut image.

--- src/tests/test_imgs.py
+++ src/tests/test_imgs.py
@@ -1,19 +1,33 @@
-from meterviewer.img import show_img
-from meterviewer.imgs import draw
+from meterviewer import img
+from meterviewer.imgs import draw, cut
 from meterviewer.datasets import imgv
 from meterviewer import types as T
 from pathlib import Path as P
+
+
+def test_cut(root_path):
+    img_path = root_path / "lens_6/XL/XL/M1L3XL/2018-11-23-12-16-01.jpg"
+    im = img.plt.imread(img_path)
+
+    """<xmin>82</xmin>
+			<ymin>39</ymin>
+			<xmax>112</xmax>
+			<ymax>72</ymax>"""
+
+    rect = T.Rect(xmin="82", ymin="39", xmax="112", ymax="72")
+    cut_img = cut.cut_img(im, rect)
+    img.show_img(cut_img, is_stop=0)
 
 
 def test_draw(root_path):
     img_path = root_path / "lens_6/XL/XL/M1L3XL/2018-11-23-12-16-01.jpg"
     im, v, rect = imgv.view_one_img_v(img_path)
     im = draw.draw_rectangle(im, rect)
-    show_img(im, is_stop=0)
+    img.show_img(im, is_stop=0)
 
 
 def test_draw_text(root_path):
     img_path = root_path / "lens_6/XL/XL/M1L3XL/2018-11-23-12-16-01.jpg"
     im, v, _ = imgv.view_one_img_v(img_path)
     im = draw.draw_text(im, v)
-    show_img(im, is_stop=0)
+    img.show_img(im, is_stop=0)
