4ec64b1c22edb29c685a174eb7a6d87d82bd661f
svtter <svtter@qq.com>
2024-05-24 16:02:48 +0800

    feat: add test-create.

---
+++ src/tests/test_cmd.py
@@ -0,0 +1,8 @@
+from meterviewer.cmd import create_all
+
+
+def test_create(root_path):
+    # this script is used for only once.
+    if (root_path / "alldata.db").exists():
+        return
+    create_all.main(root_path)
