32649561363a23ceb122df87ab05c014bf63a68e
svtter <svtter@qq.com>
2024-06-06 15:35:42 +0800

    feat: use new root-path config.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 import pathlib
-import platform
 import typing as t
 from pathlib import Path as P
+from meterviewer.config import get_root_path
 
 
 @pytest.fixture
@@ -24,30 +24,4 @@ def remove_files() -> t.Generator[t.Callable[[P], None], None, None]:
 
 @pytest.fixture
 def root_path() -> pathlib.Path:
-    def get_system_type() -> t.Literal["macOS", "Windows", "Other", "Linux"]:
-        """
-        判断当前系统类型是 macOS 还是 Windows。
-
-        Returns:
-            str: 'macOS', 'Windows' 或 'Other'，分别表示 macOS 系统、Windows 系统或其他系统。
-        """
-        system = platform.system()
-
-        if system == "Darwin":
-            return "macOS"
-        elif system == "Windows":
-            return "Windows"
-        elif system == "Linux":
-            return "Linux"
-        else:
-            return "Other"
-
-    if get_system_type() == "macOS":
-        # expand userhome
-        return pathlib.Path("/Volumes/Fast SSD/Research/Dataset/MeterData")
-    elif get_system_type() == "Windows":
-        return pathlib.Path(r"D:\Store\MeterData")
-    elif get_system_type() == "Linux":
-        return pathlib.Path("~/Work/Dataset/MeterData").expanduser()
-    else:
-        raise Exception("Not work under Linux now. 2024-05-21.")
+    return get_root_path()
