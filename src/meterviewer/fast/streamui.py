24d2e7e2e4ec0ba45810a704825e0dd2d6a8c79f
svtter <svtter@qq.com>
2024-06-12 17:21:29 +0800

    feat: use streamlit build a quick-viewer. It's good!

---
+++ src/meterviewer/fast/streamui.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from meterviewer.config import get_root_path
+
+
+def our_app():
+    st.title("Quick Viewer of Meterdata")
+    # path = st.text_input("Enter the path of the data")
+    num = st.text_input("Enter the number of data")
+    try:
+        data_load_state = st.text("reading data...")
+        num = int(num)
+        data_load_state.text("Done!")
+    except ValueError:
+        data_load_state.text("Please enter a number")
+        return
+
+    root_path = get_root_path()
+    x_path = root_path / "generated_merged" / "x_test.npy"
+    y_path = root_path / "generated_merged" / "y_test.npy"
+
+    x = np.load(x_path)
+    y = np.load(y_path)
+
+    st.image(x[num], caption=f"Meterdata {num}")
+    st.text(f"Meterdata {num} is {y[num]}")
+
+
+def example():
+    st.title("Uber pickups in NYC")
+    DATE_COLUMN = "date/time"
+    DATA_URL = (
+        "https://s3-us-west-2.amazonaws.com/"
+        "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+    )
+
+    @st.cache_data
+    def load_data(nrows):
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis="columns", inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
+
+    data_load_state = st.text("Loading data...")
+    data = load_data(10000)
+    data_load_state.text("Done! (using st.cache_data)")
+
+    if st.checkbox("Show raw data"):
+        st.subheader("Raw data")
+        st.write(data)
+
+    st.subheader("Number of pickups by hour")
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+    st.bar_chart(hist_values)
+
+    # Some number in the range 0-23
+    hour_to_filter = st.slider("hour", 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+    st.subheader("Map of all pickups at %s:00" % hour_to_filter)
+    st.map(filtered_data)
+
+
+our_app()
