98fa86907655804651dc6dbf609fe77444dd4a08
svtter <svtter@qq.com>
2024-06-28 15:46:37 +0800

    feat add get hash.

--- src/meterviewer/datasets/imgv.py
+++ src/meterviewer/datasets/imgv.py
@@ -1,11 +1,17 @@
 """view image and value"""
 
 import typing as t
-from meterviewer.datasets import config
-from meterviewer import T
-from PIL import ImageFile
 from pathlib import Path as P
+
+from loguru import logger
 from matplotlib import pyplot as plt
+from PIL import ImageFile
+
+from meterviewer import T
+from meterviewer.datasets import config
+from meterviewer.img import cmp
+
+logger.add("./logs/meterviewer-proc.log")
 
 # 修复 PIL 无法读取截断图片的问题; matplotlib 底层居然是 PIL？是我搞错了吗？
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -16,3 +22,21 @@ def view_one_img_v(filepath: P) -> t.Tuple[T.Img, str, T.Rect]:
     im = plt.imread(str(filepath))
     v, rect = config.get_xml_config(filepath)
     return (im, v, rect)
+
+
+hash_store = {}
+
+
+def save_hash(im: T.Img, index: int):
+    hash = cmp.get_hash(im)
+    res = hash_store.get(hash, None)
+    if res is None:
+        hash_store[hash] = index
+    else:
+        logger.error(f"Hash: {hash} is same with {res} and {index}")
+        raise Exception("Img should not be same.")
+
+
+def find_files(img_list: T.ImgList):
+    for i, img in enumerate(img_list):
+        save_hash(img, i)
--- src/meterviewer/img/cmp.py
+++ src/meterviewer/img/cmp.py
@@ -3,7 +3,9 @@ from meterviewer import types as T
 import hashlib
 
 
-def comp_ims(im1: T.Img, im2: T.Img):
-    def compute_(im):
-        return hashlib.md5(im.tobytes()).digest()
-    return compute_(im1) == compute_(im2)
+def comp_ims(im1: T.Img, im2: T.Img) -> bool:
+    return get_hash(im1) == get_hash(im2)
+
+
+def get_hash(img: T.Img) -> bytes:
+    return hashlib.md5(img.tobytes()).digest()
