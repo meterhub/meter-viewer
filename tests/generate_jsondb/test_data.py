a0ad9617ac505e33355bbab862fde6b861a5950f
修昊 <github@svtter.cn>
2024-12-25 15:26:33 +0800

    support relative path. (#6)

--- tests/generate_jsondb/test_data.py
+++ tests/generate_jsondb/test_data.py
@@ -1,8 +1,12 @@
+import os
 import pathlib
 
-from meterviewer.generator.jsondb import get_random_data
+from meterviewer.generator.jsondb import get_base_dir, get_random_data
 
 
 def test_get_random_data(set_config):
-  data = get_random_data()
+  data = get_random_data(is_relative_path=False)
   assert pathlib.Path(data).exists()
+
+  data_path = get_random_data(is_relative_path=True)
+  assert os.path.exists(get_base_dir() / data_path)
